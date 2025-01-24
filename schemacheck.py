d48195a0d67d7d626acfacbe40ee263aba422236
Milosz Wasilewski <milosz.wasilewski@foundries.io>
2025-01-24 12:58:11 +0000

    Add more verbose output to schema validator
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@foundries.io>

--- schemacheck.py
+++ schemacheck.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import yaml
+import voluptuous
 from lava_common.schemas import validate
 
 exitcode = 0
@@ -15,8 +16,9 @@ for root, dirs, files in os.walk("./lava"):
                 y = yaml.safe_load(f)
                 f.close()
                 validate(y)
-            except:
+            except voluptuous.Invalid as exc:
                 print(f"{filename} is invalid")
+                print(exc.msg)
                 exitcode += 1
             print(f"{filename} is valid")
 sys.exit(exitcode)
