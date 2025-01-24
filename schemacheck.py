f728e3033ba16be34eaf928401336684ff1ea141
Milosz Wasilewski <milosz.wasilewski@gmail.com>
2025-01-24 12:49:23 +0000

    Add schemacheck.py and update lava-sanity action
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@gmail.com>

---
+++ schemacheck.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import yaml
+from lava_common.schemas import validate
+
+exitcode = 0
+
+for root, dirs, files in os.walk("./lava"):
+    for fname in files:
+        if fname.endswith(".yaml"):
+            filename = os.path.join(root, fname)
+
+            try:
+                f = open(filename, "rb")
+                y = yaml.safe_load(f)
+                f.close()
+                validate(y)
+            except:
+                print(f"{filename} is invalid")
+                exitcode += 1
+            print(f"{filename} is valid")
+sys.exit(exitcode)
+
